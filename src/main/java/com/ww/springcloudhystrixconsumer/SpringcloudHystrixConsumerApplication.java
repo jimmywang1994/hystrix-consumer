6693b2cf27d8b3b47f087c3291f4a8f1cf8570c7
wwei <429744781@qq.com>
2019-10-20 09:51:15 +0800

    代码提交，雪崩处理

---
+++ src/main/java/com/ww/springcloudhystrixconsumer/SpringcloudHystrixConsumerApplication.java
@@ -0,0 +1,27 @@
+package com.ww.springcloudhystrixconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @EnableCircuitBreaker - 开启断路器。就是开启hystrix服务容错能力。
+ * 当应用启用Hystrix服务容错的时候，必须增加的一个注解。
+ */
+@EnableCircuitBreaker
+/**
+ * @EnableCaching - 开启spring cloud对cache的支持。
+ * 可以自动的使用请求缓存，访问redis等cache服务。
+ */
+@EnableCaching
+@EnableEurekaClient
+@SpringBootApplication
+public class SpringcloudHystrixConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudHystrixConsumerApplication.class, args);
+    }
+
+}
